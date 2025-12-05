e799df1a34176235fbe6dbf03a7f8e83e9d2c46b
aryan5333 <sinhaaryan5333@gmail.com>
2025-12-05 07:59:28 +0000

    update

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const AccountId=45533
+let accountEmail="Aryan0102@gmail.com"
+var accountPassword="56987"
+accountCity="jamshedpur"
+
+//AccountId=2
+accountEmail="ar@ar.com"
+accountPassword="1234"
+accountCity="ranchi"
+//console.log(AccountId);
+/*
+prefer not use var because of issue in block scope and fuvtional scope
+*/
+console.table([AccountId,accountEmail,accountPassword,accountCity])
