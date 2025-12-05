9e134ebce80bd515894bcfc09af06d21c8e1a3f1
aryan5333 <sinhaaryan5333@gmail.com>
2025-12-05 11:47:55 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all the js code as the newer version
+
+//alert (3+3)//we are using node.js,not browser//alert ke baare me padhne ke liye https://tc39.es/ecma262/#sec-global-object
+
+console.log(3 + 3) //code readibility should be high
+
+console.log("Aryan")
+
+let name="Aryan"
+let age=23
+let state;
+
+/* dataTypes-
+number= range 2 to power 53
+bigint
+String=>""
+boolean=>true/false
+null=>standalone value
+undefined=>
+symbol=>unique
+
+object
+*/
+
+console.log(typeof "Aryan")
+console.log(typeof 23)
