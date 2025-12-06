ad15cb1f534359a6fe05214194312d1f29b6ff22
aryan5333 <sinhaaryan5333@gmail.com>
2025-12-06 07:46:19 +0000

    numbers

---
+++ 01_basics/06_Numbersandmaths.js
@@ -0,0 +1,49 @@
+// let score=400
+// console.log(score);
+
+//let balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(3));
+
+// let theNumber=193.666
+// console.log(theNumber.toPrecision(2));
+
+// const Numb=100000000
+// console.log(Numb.toLocaleString('en-IN'));
+
+//****************************************Maths****************************************************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.min(4,3,2,5,68));
+// console.log(Math.max(9,8,4,75,3))
+
+// console.log(Math.random());//it basically gives no.btw 0and 1
+// console.log((Math.random()*10)+1);
+
+// const min=10
+// const max=40
+
+// console.log(Math.floor(Math.random()*(max-min +1))+min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
